fc9ffcb171164d9040e736e137104cf33ea601c4
Levente <leventefabry@gmail.com>
2018-01-29 18:32:30 +0100

    upload

--- TicTacToe/Form1.Designer.cs
+++ TicTacToe/Form1.Designer.cs
@@ -28,12 +28,123 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.XLabel = new System.Windows.Forms.Label();
+            this.NewGameBtn = new System.Windows.Forms.Button();
+            this.XLabelCount = new System.Windows.Forms.Label();
+            this.OLabelCount = new System.Windows.Forms.Label();
+            this.OLabel = new System.Windows.Forms.Label();
+            this.DrawLabelCount = new System.Windows.Forms.Label();
+            this.DrawLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // XLabel
+            // 
+            this.XLabel.AutoSize = true;
+            this.XLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.XLabel.Location = new System.Drawing.Point(12, 55);
+            this.XLabel.Name = "XLabel";
+            this.XLabel.Size = new System.Drawing.Size(24, 20);
+            this.XLabel.TabIndex = 0;
+            this.XLabel.Text = "X:";
+            this.XLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // NewGameBtn
+            // 
+            this.NewGameBtn.Location = new System.Drawing.Point(12, 29);
+            this.NewGameBtn.Name = "NewGameBtn";
+            this.NewGameBtn.Size = new System.Drawing.Size(75, 23);
+            this.NewGameBtn.TabIndex = 1;
+            this.NewGameBtn.Text = "New Game";
+            this.NewGameBtn.UseVisualStyleBackColor = true;
+            this.NewGameBtn.Click += new System.EventHandler(this.NewGameBtn_Click);
+            // 
+            // XLabelCount
+            // 
+            this.XLabelCount.AutoSize = true;
+            this.XLabelCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.XLabelCount.Location = new System.Drawing.Point(69, 55);
+            this.XLabelCount.Name = "XLabelCount";
+            this.XLabelCount.Size = new System.Drawing.Size(18, 20);
+            this.XLabelCount.TabIndex = 2;
+            this.XLabelCount.Text = "0";
+            this.XLabelCount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // OLabelCount
+            // 
+            this.OLabelCount.AutoSize = true;
+            this.OLabelCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.OLabelCount.Location = new System.Drawing.Point(69, 79);
+            this.OLabelCount.Name = "OLabelCount";
+            this.OLabelCount.Size = new System.Drawing.Size(18, 20);
+            this.OLabelCount.TabIndex = 4;
+            this.OLabelCount.Text = "0";
+            this.OLabelCount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // OLabel
+            // 
+            this.OLabel.AutoSize = true;
+            this.OLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.OLabel.Location = new System.Drawing.Point(12, 79);
+            this.OLabel.Name = "OLabel";
+            this.OLabel.Size = new System.Drawing.Size(25, 20);
+            this.OLabel.TabIndex = 3;
+            this.OLabel.Text = "O:";
+            this.OLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // DrawLabelCount
+            // 
+            this.DrawLabelCount.AutoSize = true;
+            this.DrawLabelCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.DrawLabelCount.Location = new System.Drawing.Point(69, 103);
+            this.DrawLabelCount.Name = "DrawLabelCount";
+            this.DrawLabelCount.Size = new System.Drawing.Size(18, 20);
+            this.DrawLabelCount.TabIndex = 6;
+            this.DrawLabelCount.Text = "0";
+            this.DrawLabelCount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // DrawLabel
+            // 
+            this.DrawLabel.AutoSize = true;
+            this.DrawLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.DrawLabel.Location = new System.Drawing.Point(12, 103);
+            this.DrawLabel.Name = "DrawLabel";
+            this.DrawLabel.Size = new System.Drawing.Size(50, 20);
+            this.DrawLabel.TabIndex = 5;
+            this.DrawLabel.Text = "Draw:";
+            this.DrawLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.NewGameBtn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(484, 441);
+            this.Controls.Add(this.DrawLabelCount);
+            this.Controls.Add(this.DrawLabel);
+            this.Controls.Add(this.OLabelCount);
+            this.Controls.Add(this.OLabel);
+            this.Controls.Add(this.XLabelCount);
+            this.Controls.Add(this.NewGameBtn);
+            this.Controls.Add(this.XLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Tic Tac Toe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label XLabel;
+        private System.Windows.Forms.Button NewGameBtn;
+        private System.Windows.Forms.Label XLabelCount;
+        private System.Windows.Forms.Label OLabelCount;
+        private System.Windows.Forms.Label OLabel;
+        private System.Windows.Forms.Label DrawLabelCount;
+        private System.Windows.Forms.Label DrawLabel;
     }
 }
 
--- TicTacToe/Form1.cs
+++ TicTacToe/Form1.cs
@@ -12,9 +12,184 @@ namespace TicTacToe
 {
     public partial class Form1 : Form
     {
+        private int _maxButton = 9;
+        private bool _changeUser;
+        private List<string> _xList = new List<string>();
+        private List<string> _oList = new List<string>();
+        private int _xWin = 0;
+        private int _oWin = 0;
+        private int _draw = 0;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int top = NewGameBtn.Top;
+            int left = NewGameBtn.Right + 20;
+            int size = 100;
+
+            for (int i = 0; i < _maxButton; i++)
+            {
+                var b = new Button();
+                Controls.Add(b);
+                b.Name = i.ToString();
+                b.Top = top;
+                b.Left = left;
+                b.Width = size;
+                b.Height = size;
+                //b.ForeColor = Color.Black;
+                b.Font = new Font(FontFamily.GenericSansSerif, 30f, FontStyle.Bold);
+                b.Click += GameButton_click;
+
+                if ((i + 1) % 3 == 0)
+                {
+                    left = NewGameBtn.Right + 20;
+                    top += size + 20;
+                }
+                else
+                {
+                    left += size + 20;
+                }
+            }
+        }
+
+        private void NewGame()
+        {
+            ButtonEnable(true);
+            foreach (Control item in Controls)
+            {
+                if ((item is Button) && item.Name != "NewGameBtn")
+                {
+                    item.Text = "";
+                }
+            }
+            _xList.Clear();
+            _oList.Clear();
+        }
+
+        private void ButtonEnable(bool boolean)
+        {
+            foreach (Control item in Controls)
+            {
+                if ((item is Button) && item.Name != "NewGameBtn")
+                {
+                    item.Enabled = boolean;
+                }
+            }
+        }
+
+        private void HighScore()
+        {
+            OLabelCount.Text = _oWin.ToString();
+            XLabelCount.Text = _xWin.ToString();
+            DrawLabelCount.Text = _draw.ToString();
+        }
+
+        private bool Win(List<string> list)
+        {
+            //vertical
+            if (list.Contains("0") && list.Contains("1") && list.Contains("2"))
+            {
+                return true;
+            }
+            else if (list.Contains("3") && list.Contains("4") && list.Contains("5"))
+            {
+                return true;
+            }
+            else if (list.Contains("6") && list.Contains("7") && list.Contains("8"))
+            {
+                return true;
+            }
+
+            //horizontal
+            else if (list.Contains("0") && list.Contains("3") && list.Contains("6"))
+            {
+                return true;
+            }
+            else if (list.Contains("1") && list.Contains("4") && list.Contains("7"))
+            {
+                return true;
+            }
+            else if (list.Contains("2") && list.Contains("5") && list.Contains("8"))
+            {
+                return true;
+            }
+
+            //diagonal
+            else if (list.Contains("0") && list.Contains("4") && list.Contains("8"))
+            {
+                return true;
+            }
+            else if (list.Contains("2") && list.Contains("4") && list.Contains("6"))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private int NumberOfClickedButtons()
+        {
+            int buttonNumbers = 0;
+
+            foreach (Control item in Controls)
+            {
+                if (item is Button && item.Enabled == false)
+                {
+                    buttonNumbers++;
+                }
+            }
+
+            return buttonNumbers;
+        }
+
+        private void GameButton_click(object sender, EventArgs e)
+        {
+            if (sender is Button)
+            {
+                if (!_changeUser)
+                {
+                    (sender as Button).Text = "X";
+                    (sender as Button).Enabled = false;
+                    _changeUser = true;
+                    _xList.Add((sender as Button).Name);
+                }
+                else
+                {
+                    (sender as Button).Text = "O";
+                    (sender as Button).Enabled = false;
+                    _changeUser = false;
+                    _oList.Add((sender as Button).Name);
+                }
+
+                if (Win(_xList))
+                {
+                    MessageBox.Show("X Win!");
+                    ButtonEnable(false);
+                    _xWin++;
+
+                }
+                else if (Win(_oList))
+                {
+                    MessageBox.Show("O Win!");
+                    ButtonEnable(false);
+                    _oWin++;
+                }
+                else if (NumberOfClickedButtons() == 9)
+                {
+                    MessageBox.Show("Draw");
+                    _draw++;
+                }
+                HighScore();
+            }
+        }
+
+        private void NewGameBtn_Click(object sender, EventArgs e)
+        {
+            NewGame();
         }
     }
 }
